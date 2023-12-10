bfc51368eb412f5c6fa4294cdac8314b4c051be3
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-12-11 00:09:12 +0100

    Move the tags info to the $properties

--- src/external-code/GenerateStructure/Dependencies/AddObjectTags.lua
+++ src/external-code/GenerateStructure/Dependencies/AddObjectTags.lua
@@ -2,7 +2,7 @@ local CollectionService = game:GetService("CollectionService")
 
 return function (data, instance : Instance)
     assert(type(data) == "table", "Data isnt a table can't save tags here!")    
-    data["tags"] = CollectionService:GetTags(instance)
+    data["$properties"]["tags"] = CollectionService:GetTags(instance)
 
     return data
 end
