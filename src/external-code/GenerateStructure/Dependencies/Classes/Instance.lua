08e7293b272a1468f0129893f44b6b9e7137d173
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-12-12 02:20:12 +0100

    Remove the name key because it isn't allowed

--- src/external-code/GenerateStructure/Dependencies/Classes/Instance.lua
+++ src/external-code/GenerateStructure/Dependencies/Classes/Instance.lua
@@ -2,7 +2,6 @@ return function(data, instance : Instance)
     data = data or {}
     assert(type(data) == "table", "passed in data isn't a table??")
 
-    data["Name"] = instance.Name
     data["$className"] = instance.ClassName
     data["$properties"] = {}
 
