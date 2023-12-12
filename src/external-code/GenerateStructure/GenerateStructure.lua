671a074422330ca7e9a4241e8eb5c26580431a5d
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-12-12 02:21:45 +0100

    Change the way identical names are handles

--- src/external-code/GenerateStructure/GenerateStructure.lua
+++ src/external-code/GenerateStructure/GenerateStructure.lua
@@ -2,6 +2,20 @@
 local propertyHandlers = require(script:WaitForChild("PropertyHandlers"))
 local addObjectTags = require(script:WaitForChild("AddObjectTags"))
 
+
+local function getUniqueName(name, tableWithNames)
+    if tableWithNames[name] == nil then
+        return name
+    else
+        local uniqueName = name
+        repeat
+            uniqueName = uniqueName .. '~'
+        until tableWithNames[uniqueName] == nil
+
+        return uniqueName
+    end
+end
+
 local function generateChildStruct(child : Instance)
     if propertyHandlers[child.ClassName] == nil then
         warn(string.format("There is currently no support for the instances of type [%s]\nIf you wish to feel free to contribute your own code or inform the owner of the repository to add support for this instace\nGITHUB LINK: https://github.com/EmreArapcicUevak/InstanceToRojo-Exporter", child.ClassName))
@@ -12,8 +26,7 @@ local function generateChildStruct(child : Instance)
     addObjectTags(dataForChild, child)
 
     for index : number, grandChild: Instance in pairs(child:GetChildren()) do
-        local key = string.format("%d-%s", index, grandChild.Name)
-        dataForChild[key] = generateChildStruct(grandChild)
+        dataForChild[getUniqueName(grandChild.Name, dataForChild)] = generateChildStruct(grandChild)
     end
 
     return dataForChild
@@ -31,7 +44,7 @@ return function (object : Instance)
     addObjectTags(structure["tree"], object)
 
     for index : number, child: Instance in pairs(object:GetChildren()) do
-        structure["tree"][string.format("%d-%s", index, child.Name)] = generateChildStruct(child)
+        structure["tree"][getUniqueName(child.Name, structure["tree"])] = generateChildStruct(child)
     end
 
     return structure
